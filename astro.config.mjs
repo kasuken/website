985600de49fab68325d24a4afd458ca9294d1108
Emanuele Bartolesi <bartolesiemanuele@hotmail.com>
2025-08-18 08:54:51 +0000

    feat: Add aws-amplify integration to Astro configuration and package files
    - Updated astro.config.mjs to include awsAmplify adapter for deployment.
    - Added astro-aws-amplify dependency in package.json and package-lock.json.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
+import awsAmplify from "astro-aws-amplify";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://example.com",
+  integrations: [mdx(), sitemap()],
+  adapter: awsAmplify(),
+  output: "server",
 });
